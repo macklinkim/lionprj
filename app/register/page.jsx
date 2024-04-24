8cda85860f608b11353a7bfd098f1e4a0862a84d
mackh <kopserf@gmail.com>
2024-04-24 11:52:42 +0900

    🐛

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,8 +1,9 @@
 import UserRegisterForm from "@components/UserRegisterForm";
+import { getServerSession } from "next-auth";
 // import { authOptions } from "@api/auth/[...nextauth]/route";
 
 export default async function Register() {
-	const session = await getServerSession(authOptions);
+	const session = await getServerSession();
 	if (session) redirect("/dashboard");
 
 	return <UserRegisterForm />;
